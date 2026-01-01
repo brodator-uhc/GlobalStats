4ed778798a0ce73c693352d245dc0ae85fa9b97c
William Garneau <1639782@cll.qc.ca>
2026-01-01 18:56:32 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+Console.WriteLine("Hello, World!");
+
+string filePath = "C:\\Users\\William\\Desktop\\Stats\\Global-Stats\\GlobalStats\\Global RR Stats Community Document.xlsx";
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine($"Error: File not found at {filePath}");
+    return;
+}
+
+using var workbook = new XLWorkbook(filePath);
+
+for (int sheet = 8; sheet <= workbook.Worksheets.Count; sheet++)
+{
+    var worksheet = workbook.Worksheet(sheet);
+    for (int season = 1; season <= (worksheet.ColumnCount() - 1) / 3; season++)
+    {
+
+    }
+}
+
+//Console.WriteLine($"Reading data from '{worksheet.Name}'...");
+
+// Iterate over the rows and columns
+// ClosedXML uses 1-based indexing for rows and columns
+// Read the cell value and print it to the console
+//var cellValue = worksheet.Cell(row, col).Value.ToString();
+//Console.Write(cellValue + "\t");
+
+//Console.WriteLine();
