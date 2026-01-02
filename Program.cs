f390905190dde778d06c76c0b82be349174722e9
William Garneau <1639782@cll.qc.ca>
2026-01-01 20:56:25 -0500

    Saving into a doc!

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using DocumentFormat.OpenXml.Spreadsheet;
 Console.WriteLine("Hello, World!");
 
 string filePath = "C:\\Users\\William\\Desktop\\Stats\\Global-Stats\\GlobalStats\\Global RR Stats Community Document.xlsx";
+List<String> rounds = new List<String>();
 
 if (!File.Exists(filePath))
 {
@@ -17,11 +18,20 @@ using var workbook = new XLWorkbook(filePath);
 for (int sheet = 8; sheet <= workbook.Worksheets.Count; sheet++)
 {
     var worksheet = workbook.Worksheet(sheet);
+    rounds.Add(worksheet.Name);
+    Console.WriteLine("Working on " + worksheet.Name);
     for (int season = 1; season <= (worksheet.ColumnCount() - 1) / 3; season++)
     {
-
+        
     }
 }
+
+var statscompiled = new XLWorkbook();
+var allrosters = statscompiled.AddWorksheet("All Rosters");
+allrosters.Cell("A1").InsertData(rounds);
+statscompiled.SaveAs("C:\\Users\\William\\Desktop\\Stats\\Global-Stats\\GlobalStats\\StatsCompiled.xlsx");
+Console.WriteLine("Stats are now compiled!");
+
 
 //Console.WriteLine($"Reading data from '{worksheet.Name}'...");
 
